4ddaeff65e117771ecc205f5320f4941060de0b4
Zorr0S <86990362+Zorr0S@users.noreply.github.com>
2021-09-15 16:02:06 -0600

    Pureba

--- Prueba.js
+++ Prueba.js
@@ -1,1 +1,15 @@
 console.log("Simon");
+console.log("asies");
+console.log("como vez");
+console.log("Esta vez");
+var A = /** @class */ (function () {
+    function A() {
+        this.x = 10;
+    }
+    A.prototype.ImprimirX = function (Otra) {
+        console.log(Otra.x);
+    };
+    return A;
+}());
+var b = new A();
+b.ImprimirX(new A());
--- Prueba.ts
+++ Prueba.ts
@@ -2,3 +2,15 @@ console.log("Simon");
 console.log("asies");
 console.log("como vez");
 console.log("Esta vez");
+
+class A {
+    private x= 10;
+
+    public ImprimirX(Otra: A){
+        console.log(Otra.x);
+    }
+}
+
+const b = new A();
+
+b.ImprimirX(new A());
